d7684992dc41878cf0ed5bb85482c758643cfacf
omidshj <omidshj@gmail.com>
2019-09-24 14:34:47 +0330

    test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class Test extends Component{
+  state = {
+    notes: ['first note', 'second note'],
+    name: 'dfsdf'
+  }
+
+  render(){    
+    return <div className="container mx-auto p-8">
+      <NoteAdder FF={ messagge => this.setState({notes: [...this.state.notes, messagge]}) } />
+      <div className="flex flex-wrap">
+        {this.state.notes.map(note =><NoteComponent text={note} /> )}
+      </div>
     </div>
-  );
+  }
 }
 
-export default App;
+class NoteAdder extends Component{
+  state = {text: ''}
+  render(){
+    return <div>
+      <button type="button" className="bg-gray-300 p-2" onClick={() => this.props.FF('new note')} >add note</button> <br/><br/><br/>
+    </div>
+  }
+}
+
+class NoteComponent extends Component{
+  render(){    
+    return <div className="p-8 border m-2">
+      {this.props.text}
+    </div>
+  }
+}
